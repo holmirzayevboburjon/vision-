c602edc72db5952913b4cd4d56f77e753d665b13
holmirzayevboburjon <holmirzayevboburjon2002@gmail.com>
2026-09-16 00:34:20 +0500

    Add files via upload

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.VISION_CONFIG = {
-  googleSheetWebhook: "",
+  googleSheetWebhook: "https://script.google.com/macros/s/AKfycbziDUGQ5jlAegI0dC7jUpsODzFCy2DQq9Y0N73e9g8BOnWchuyoyg3KUWxoGRNY3XMDwQ/exec",
   telegramUrl: ""
 };
